910a4b5bb9273b17b43a53afd55f96b40a0998c5
Pratik <88044632+ptk39005@users.noreply.github.com>
2025-02-12 19:24:46 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -394,7 +394,7 @@ def get_available_files(user_email):
         return []
     
     files = []
-    blobs = bucket.list_blobs(prefix=f"{user_email}/data/")
+    blobs = bucket.list_blobs(prefix=f"/")
     for blob in blobs:
         # Extract just the filename from the full path
         filename = blob.name.split('/')[-1]
